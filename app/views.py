fa62e9c03a41411de2a5df7145de02c1b798c9da
Sergio Palechor <davidpalechor95@hotmail.com>
2018-06-26 19:23:12 -0500

    Final setup

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ from .models import AdditionalInfo
 from .models import BasicInfo
 
 
-class CreateInfo(View):
+class CreateInfoView(View):
     '''
     View for testing POST method
     '''
@@ -27,18 +27,21 @@ class CreateInfo(View):
         json_response = json.loads(request.body)
 
         basic_info = BasicInfo.objects.create(
-            name=json_response['basicInfo']['name'],
-            last_name=json_response['basicInfo']['last_name'],
-            tel=json_response['basicInfo']['tel'],
-            email=json_response['basicInfo']['email'],
-            address=json_response['basicInfo']['address']
+            name=json_response['name'],
+            last_name=json_response['last_name'],
+            tel=json_response['tel'],
+            email=json_response['email'],
+            address=json_response['address']
         )
 
-        if json_response['additionalInfo']:
+        if (json_response['art'] or
+            json_response['movies'] or
+            json_response['music']):
+
             AdditionalInfo.objects.create(
-                art=json_response['additionalInfo'].get('art'),
-                movies=json_response['additionalInfo'].get('movies'),
-                music=json_response['additionalInfo'].get('music'),
+                art=json_response['art'],
+                movies=json_response['movies'],
+                music=json_response['music'],
                 basic_info=basic_info,
             )
 
@@ -49,6 +52,7 @@ class InfoListView(ListView):
     '''
     View for testing GET method
     '''
+
     model = BasicInfo
 
     @method_decorator(csrf_exempt)
@@ -67,10 +71,60 @@ class InfoListView(ListView):
                 'address': info.email,
             })
 
-        return JsonResponse({'data': data}, status=200)
+        return JsonResponse({'data': data}, status=201)
 
     def post(self, request):
         return HttpResponse(
             'Method not allowed, request via GET instead',
             status=405
         )
+
+
+class UpdateInfoView(APIView):
+    '''
+    View for testing PUT method
+    '''
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def put(self, request):
+        json_response = json.loads(request.body)
+
+        if BasicInfo.objects.filter(email=json_response['email']).exists():
+            info = BasicInfo.objects.get(email=json_response['email'])
+            info.name = json_response['name']
+            info.last_name = json_response['last_name']
+            info.tel = json_response['tel']
+            info.address = json_response['address']
+            info.save()
+
+            if AdditionalInfo.objects.filter(basic_info=info).exists():
+                AdditionalInfo.objects.filter(basic_info=info).update(
+                    art=json_response['art'],
+                    movies=json_response['movies'],
+                    music=json_response['music']
+                )
+
+            return HttpResponse('Success', status=200)
+        return HttpResponse('Object not found', status=404)
+
+
+class DeleteInfoView(APIView):
+    '''
+    View for testing DELETE method
+    '''
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def delete(self, request):
+        json_response = json.loads(request.body)
+        print(json_response)
+        if BasicInfo.objects.filter(email=json_response['email']).exists():
+            BasicInfo.objects.get(email=json_response['email']).delete()
+
+            return HttpResponse('Success', status=200)
+        return HttpResponse('Object not found', status=404)
